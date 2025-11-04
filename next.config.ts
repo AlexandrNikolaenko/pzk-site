3901e089382d78f63a0bdb8abf9740d6a05a5f8d
Александр <nikol.alex06@mail.ru>
2025-11-04 16:59:04 +0300

    fix: remove gh actions

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  reactStrictMode: true,
-  assetPrefix: "/pzk-site/",
-  basePath: "/pzk-site",
-  images: {unoptimized: true},
 };
 
 export default nextConfig;
