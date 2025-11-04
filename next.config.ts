5c4a257d7e88d2c0d8b4b63642803ac1ef3eba69
Александр <nikol.alex06@mail.ru>
2025-11-04 16:31:31 +0300

    fix: prefix

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
-const isDev = process.env.NODE_ENV === 'development';
-
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
-  assetPrefix: isDev ? '/pzk-site/' : undefined,
-  basePath: isDev ? '/pzk-site' : undefined,
+  assetPrefix: '/pzk-site/',
+  basePath: '/pzk-site',
 };
 
 export default nextConfig;
