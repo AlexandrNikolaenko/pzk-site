ddfa74a9d78da6b9132dc64d36064448e9eee176
Александр <nikol.alex06@mail.ru>
2025-11-04 16:20:06 +0300

    init gh action

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  reactStrictMode: true,
+  assetPrefix: isProd ? '/pzk-site/' : undefined,
+  basePath: isProd ? '/pzk-site' : undefined,
 };
 
 export default nextConfig;
