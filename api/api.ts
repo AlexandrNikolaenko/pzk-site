fa6e251c7230bcf0d6a936aada50b240d9f68c53
Александр <nikol.alex06@mail.ru>
2025-11-13 19:42:12 +0300

    feat: add api and images

---
+++ api/api.ts
@@ -0,0 +1,136 @@
+class HttpRequest {
+  #api_host: string;
+
+  constructor() {
+    this.#api_host = "http://localhost:5000";
+  }
+
+  async get({ onError, onSuccess, query }: GetQuery & {query: string}) {
+    try {
+      const res = await fetch(this.#api_host.concat(query), {
+        method: "GET",
+        cache: "no-cache",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        if (onSuccess) onSuccess(data);
+        return data;
+      } else throw new Error(String(res.status));
+    } catch (e) {
+      console.log(e);
+      if (onError) onError(e);
+      return;
+    }
+  }
+
+  async post({ onError, onSuccess, body, query }: PostQuery & {query: string}) {
+    try {
+      const res = await fetch(this.#api_host.concat(query), {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      if (res.ok) {
+        const data = await res.json();
+        if (onSuccess) onSuccess(data);
+        return data;
+      } else throw new Error(String(res.status));
+    } catch (e) {
+      console.log(e);
+      if (onError) onError(e);
+      return;
+    }
+  }
+
+  async put({ onError, onSuccess, body, query }: PostQuery & {query: string}) {
+    try {
+      const res = await fetch(this.#api_host.concat(query), {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      if (res.ok) {
+        const data = await res.json();
+        if (onSuccess) onSuccess(data);
+        return data;
+      } else throw new Error(String(res.status));
+    } catch (e) {
+      console.log(e);
+      if (onError) onError(e);
+      return;
+    }
+  }
+
+  async patch({ onError, onSuccess, body, query }: PostQuery & {query: string}) {
+    try {
+      const res = await fetch(this.#api_host.concat(query), {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      if (res.ok) {
+        const data = await res.json();
+        if (onSuccess) onSuccess(data);
+        return data;
+      } else throw new Error(String(res.status));
+    } catch (e) {
+      console.log(e);
+      if (onError) onError(e);
+      return;
+    }
+  }
+
+  async delete({ onError, onSuccess, query }: GetQuery & {query: string}) {
+    try {
+      const res = await fetch(this.#api_host.concat(query), {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.ok) {
+        const data = await res.json();
+        if (onSuccess) onSuccess(data);
+        return data;
+      } else throw new Error(String(res.status));
+    } catch (e) {
+      console.log(e);
+      if (onError) onError(e);
+      return;
+    }
+  }
+}
+
+interface PostQuery {
+  body: object;
+  onSuccess: (data: object) => void;
+  onError: (error: unknown) => void;
+}
+
+interface GetQuery {
+  onSuccess: (data: object) => void;
+  onError: (error: unknown) => void;
+}
+
+export default class Api {
+  #httpRequest: HttpRequest;
+
+  constructor() {
+    this.#httpRequest = new HttpRequest();
+  }
+
+  async addLead({body, onSuccess, onError}: PostQuery) {
+    return await this.#httpRequest.post({
+      query: '/createlead',
+      body,
+      onSuccess,
+      onError
+    })
+  }
+}
