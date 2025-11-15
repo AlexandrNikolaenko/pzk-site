8673ad7e0b6571ba27a6a5401d6a69cef915d019
Александр <nikol.alex06@mail.ru>
2025-11-15 17:25:28 +0300

    feat: add logo in png

--- components/core/logo.tsx
+++ components/core/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <div className="relative w-[101px] h-[46px] max-[550px]:w-16 max-[550px]:h-[29px]">
-      <Image alt="logo" src={"/logo.svg"} fill />
+      <Image alt="logo" src={"/logo.png"} fill />
     </div>
   );
 }
