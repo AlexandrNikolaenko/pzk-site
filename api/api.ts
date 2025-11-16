c055a1c208786490963633f499f6252cff950db3
Александр <nikol.alex06@mail.ru>
2025-11-16 16:51:44 +0300

    fix: domen

--- api/api.ts
+++ api/api.ts
@@ -4,7 +4,7 @@ class HttpRequest {
   #api_host: string;
 
   constructor() {
-    this.#api_host = "http://178.72.150.74/api";
+    this.#api_host = "https://spb.pzkgroup.ru//api";
   }
 
   async get({ onError, onSuccess, query }: GetQuery & { query: string }) {
