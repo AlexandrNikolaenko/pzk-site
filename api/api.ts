71fa1584fa003c3345eed28abab8164ed8b9854e
Александр <nikol.alex06@mail.ru>
2025-12-13 18:53:34 +0300

    fix: timeout of connection

--- api/api.ts
+++ api/api.ts
@@ -32,21 +32,13 @@ class HttpRequest {
     query,
   }: PostQuery & { query: string }) {
     try {
-      const controller = new AbortController();
-      const signal = controller.signal;
-
-      const timeoutId = setTimeout(() => {
-        controller.abort(); // Прерываем запрос при таймауте
-      }, 2 * 60 * 1000);
       const res = await fetch(this.#api_host.concat(query), {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(body),
-        signal
       });
-      clearTimeout(timeoutId);
       if (res.ok) {
         const data = await res.json();
         if (onSuccess) onSuccess(data);
@@ -66,10 +58,18 @@ class HttpRequest {
     query,
   }: PostQuery & { query: string, body: FormData }) {
     try {
+      const controller = new AbortController();
+      const signal = controller.signal;
+
+      const timeoutId = setTimeout(() => {
+        controller.abort(); // Прерываем запрос при таймауте
+      }, 2 * 60 * 1000);
       const res = await fetch(this.#api_host.concat(query), {
         method: "POST",
         body: body,
+        signal
       });
+      clearTimeout(timeoutId);
       if (res.ok) {
         const data = await res.json();
         if (onSuccess) onSuccess(data);
