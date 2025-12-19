29347cac40162e3fed345bafc2d6576a53ce549d
Александр <nikol.alex06@mail.ru>
2025-12-19 18:23:03 +0300

    fix: fix size in input

--- components/form/input.tsx
+++ components/form/input.tsx
@@ -8,7 +8,7 @@ export default function InputField({
     <input
       {...props}
       autoComplete="on"
-      className={`py-[18px] px-[30px] max-md:py-[11px] max-md:px-[19px] bg-[#B6B6B6] placeholder:text-[#7A7A7A] placeholder:text-xl placeholder:max-md:text-xs placeholder:leading-[115.1%] placeholder:tracking-[-.02em] rounded-full w-full ${isError && "error"}`}
+      className={`py-[18px] px-[30px] max-md:py-[11px] max-md:px-[19px] bg-[#B6B6B6] placeholder:text-[#7A7A7A] placeholder:text-xl placeholder:max-md:text-base placeholder:leading-[115.1%] placeholder:tracking-[-.02em] rounded-full w-full ${isError && "error"}`}
     />
   );
 }
